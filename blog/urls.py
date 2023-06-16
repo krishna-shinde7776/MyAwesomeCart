6a0d27690840b6db497b02b66bec077d40f2ab8b
krishna-shinde7776 <krishnashinde7776@gmail.com>
2023-06-16 13:23:13 +0530

    adding new functionlity of blog app

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="index"),
+    path('', views.index,name="BlogHome"),
+    path('blogpost/<int:id>', views.blogpost,name="blogHome"),
 ]
+
